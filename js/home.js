f0b92fbbcfd52bccf525d29beb5992c3df0e15e2
newnag <newnagtl@gmail.com>
2021-08-13 13:11:29 +0700

    13/8/2021 noon

--- js/home.js
+++ js/home.js
@@ -6,11 +6,14 @@ let swiperhome = new Swiper('.home-slide',{
 });
 
 let swiper_choose = new Swiper('.slide-chooseus',{
-  freeMode: true,
   spaceBetween: 10,
   breakpoints: {
     480:{
       slidesPerView: 1,
+      slidesPerColumn: 1,
+    },
+    1366:{
+      slidesPerView: 2,
       slidesPerColumn: 1,
     },
   },
